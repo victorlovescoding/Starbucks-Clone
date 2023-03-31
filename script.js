f78e8747977a2e53bfb3cb6bb2f99954903096a3
Victorchen <96483328+victorlovescoding@users.noreply.github.com>
2023-03-31 18:21:49 +0800

    Add prettier,ESlint,and restructure css with sass

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
-const navs=Array.from(document.querySelectorAll('header nav ul li a'))
-const subMenus=Array.from(document.querySelectorAll('.sub_menu'))
-const footers=Array.from(document.querySelectorAll('.footer_main .ul_wrap ul'))
-const ul_wraps=Array.from(document.querySelectorAll('.ul_wrap'))
-const burgerIcon=document.querySelector('.burger_icon')
-const mobileNav=document.querySelector('.mobile_nav')
-const angleDown=Array.from(document.querySelectorAll('footer .footer_main .ul_wrap .fa-angle-down'))
+const navs=Array.from(document.querySelectorAll("header nav ul li a"));
+const subMenus=Array.from(document.querySelectorAll(".sub_menu"));
+const footers=Array.from(document.querySelectorAll(".footer_main .ul_wrap ul"));
+const ul_wraps=Array.from(document.querySelectorAll(".ul_wrap"));
+const burgerIcon=document.querySelector(".burger_icon");
+const mobileNav=document.querySelector(".mobile_nav");
 
-burgerIcon.addEventListener('click',()=>{
-    mobileNav.classList.toggle('hide_menu')
-})
+burgerIcon.addEventListener("click",()=>{
+	mobileNav.classList.toggle("hide_menu");
+});
 if(window.innerWidth < "768"){
-    ul_wraps.forEach((ul_wrap)=>{
-        let index=ul_wraps.indexOf(ul_wrap)
-        ul_wrap.addEventListener('click',()=>{
-            //footers[index].style.display='block'
-            if(footers[index].style.display=='none'){
-                footers[index].style.display='block'
+	ul_wraps.forEach((ul_wrap)=>{
+		let index=ul_wraps.indexOf(ul_wrap);
+		ul_wrap.addEventListener("click",()=>{
+			//footers[index].style.display='block'
+			if(footers[index].style.display=="none"){
+				footers[index].style.display="block";
                 
-            }else{
-                footers[index].style.display='none'
+			}else{
+				footers[index].style.display="none";
                 
-            }
-        })
-    })
+			}
+		});
+	});
 }
 
 
 navs.forEach((nav)=>{
-    let index=navs.indexOf(nav)
-    nav.addEventListener('mouseover',()=>{
-        nav.style.backgroundColor="#1E1E1E"
-        nav.style.color="#ffffff"
-        subMenus[index].style.display="block"
-        subMenus[index].addEventListener('mouseover',()=>{
-            nav.style.backgroundColor="#1E1E1E"
-            nav.style.color="#ffffff"
-            subMenus[index].style.display="block"
-        })
-    })
+	let index=navs.indexOf(nav);
+	nav.addEventListener("mouseover",()=>{
+		nav.style.backgroundColor="#1E1E1E";
+		nav.style.color="#ffffff";
+		subMenus[index].style.display="block";
+		subMenus[index].addEventListener("mouseover",()=>{
+			nav.style.backgroundColor="#1E1E1E";
+			nav.style.color="#ffffff";
+			subMenus[index].style.display="block";
+		});
+	});
     
-    nav.addEventListener('mouseout',()=>{
-        nav.style.backgroundColor="#F7F7F7"
-        nav.style.color="#1E1E1E"
-        subMenus[index].style.display="none"
-        subMenus[index].addEventListener('mouseout',()=>{
-            nav.style.backgroundColor="#F7F7F7"
-            nav.style.color="#1E1E1E"
-            subMenus[index].style.display="none"
-        })  
-    })
+	nav.addEventListener("mouseout",()=>{
+		nav.style.backgroundColor="#F7F7F7";
+		nav.style.color="#1E1E1E";
+		subMenus[index].style.display="none";
+		subMenus[index].addEventListener("mouseout",()=>{
+			nav.style.backgroundColor="#F7F7F7";
+			nav.style.color="#1E1E1E";
+			subMenus[index].style.display="none";
+		});  
+	});
     
-    
-})
+       
+});
 
-window.addEventListener('scroll',()=>{
-    console.log(window.scrollY);
-})
+window.addEventListener("scroll",()=>{
+	console.log(window.scrollY);
+});
